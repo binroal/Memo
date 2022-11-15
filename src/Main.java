7ec2be4a5afa02ba46644a3d0aeb0c5993338daa
binroal <binroal@github.com>
2022-11-15 11:32:35 +0900

    a

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("이경빈");
+        System.out.println("신건우");
         int a = 1;
         int b = 2;
         System.out.println(a+b);
