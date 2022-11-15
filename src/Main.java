408c45e69d2ccd081023f1a40806aa6a2a557785
binroal <binroal@github.com>
2022-11-15 11:31:08 +0900

    a

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("이경빈");
+        int a = 1;
+        int b = 2;
+        System.out.println(a+b);
+    }
+}
